cdfa36ef7fb38dc79f790ebc2a417d433df23fae
Imkbhat <kbs71190@gmail.com>
2019-06-10 11:01:15 +0530

    Updates

---
+++ precache-manifest.44876eb597ed1e880226374b5ad4a892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7061be30223d32de528662498dde4794",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "6bebd1a35ea8db2fa5ed",
+    "url": "/MyPortfolio/static/css/main.7b09a9d8.chunk.css"
+  },
+  {
+    "revision": "b3759e3641c6757bd872",
+    "url": "/MyPortfolio/static/js/2.1b8d1898.chunk.js"
+  },
+  {
+    "revision": "6bebd1a35ea8db2fa5ed",
+    "url": "/MyPortfolio/static/js/main.eef53cac.chunk.js"
+  },
+  {
+    "revision": "41ef2c5e8e68d7a2a057",
+    "url": "/MyPortfolio/static/js/runtime~main.ccce04bf.js"
+  },
+  {
+    "revision": "16df725cd65998406e58f938b65bca0c",
+    "url": "/MyPortfolio/static/media/Mine.16df725c.png"
+  }
+]);
