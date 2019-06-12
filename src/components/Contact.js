b3f966e2ae9ec96f4aff9f42ed6b537cb95f4fa3
Imkbhat <kbs71190@gmail.com>
2019-06-12 11:30:42 +0530

    Add existing file

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,54 @@
 import React, { Component } from "react";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
+import MyImage from "../image/Mine.png";
 
 class Contact extends Component {
   render() {
     return (
-      <div>
-        <h1>Contact</h1>
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Kiran B S</h2>
+            <img src={MyImage} alt="Kiran B S" style={{ height: "250px" }} />
+            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+              Software Developer with 6 years of Expirience, in Java, Spring,
+              Hibernate, Javascript, Postgresql, React(1 Year)
+            </p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+
+            <div className="conntact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "30px", fontFamily: "Anton" }}
+                  >
+                    <i className="fa fa-phone-square" aria-hidden="true" />
+                    &nbsp;9740594382
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "30px", fontFamily: "Anton" }}
+                  >
+                    <i className="fa fa-envelope" aria-hidden="true" />
+                    &nbsp;kbs71190@gmail.com
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "30px", fontFamily: "Anton" }}
+                  >
+                    <i className="fa fa-skype" aria-hidden="true" />
+                    &nbsp;Skype Id: kbs71190
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,183 @@
 import React, { Component } from "react";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardActions,
+  CardMenu,
+  IconButton,
+  Button,
+  CardText
+} from "react-mdl";
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: 0
+    };
+  }
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div className="projects-grid">
+          {/* Project 1 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(http://is3.mzstatic.com/image/thumb/Purple111/v4/85/04/a6/8504a63b-25c0-ac8e-85ab-34abd470f5e0/source/512x512bb.jpg) center / cover"
+              }}
+            >
+              Burger Builder
+            </CardTitle>
+            <CardText>
+              <ul>
+                <li>
+                  A Burger builder Application built using React Js and ReduxJs.
+                </li>
+                <li>
+                  {" "}
+                  The application is a single page application with proper
+                  components and is Mobile Responsive.
+                </li>
+                <li>
+                  The Application also used proper authentication and from
+                  validation and implements routing uses React Router.
+                </li>
+              </ul>
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          {/* Project 2 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://i1.wp.com/metro.co.uk/wp-content/uploads/2018/02/ios_weather_icons-e1519660727179.png?quality=90&strip=all&zoom=1&resize=644%2C483&ssl=1) center / cover"
+              }}
+            >
+              Find Weather
+            </CardTitle>
+            <CardText>
+              <ul>
+                <li>Find Weather Application built using React Js.</li>
+                <li>
+                  The application uses all the new features introduced in
+                  EcmaScript 6.
+                </li>
+                <li>
+                  {" "}
+                  The application is a single page application with proper
+                  components.
+                </li>
+              </ul>
+            </CardText>
+            <CardActions border>
+              <Button
+                colored
+                href="https://github.com/Imkbhat/weather-app"
+                target="_blank"
+              >
+                GitHub
+              </Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          {/* Project 3 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://pinchofyum.com/wp-content/uploads/Moroccan-Chickpea-Bowls-Recipe.jpg) center / cover"
+              }}
+            >
+              Recipe Search
+            </CardTitle>
+            <CardText>
+              <ul>
+                <li>Recipe Search Application built using React Js.</li>
+                <li>
+                  The application uses all the new features introduced in
+                  EcmaScript 6.
+                </li>
+                <li>
+                  {" "}
+                  The application is a single page application with proper
+                  components.
+                </li>
+              </ul>
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1>This is Angular</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div>
+          <h1>This is VueJS</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 3) {
+      return (
+        <div>
+          <h1>This is MongoDB</h1>
+        </div>
+      );
+    }
+  }
   render() {
     return (
       <div>
-        <h1>Projects</h1>
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>Angular</Tab>
+          <Tab>VueJS</Tab>
+          <Tab>MongoDB</Tab>
+        </Tabs>
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
